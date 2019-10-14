96a9a792d6896dcd205ca6cd7bbd311b12bd1e1d
Cholatis Srikanthimaraks <cholatissrikanthimaraks@Cholatiss-MacBook-Pro.local>
2019-10-14 21:02:07 +0700

    react native element install

--- rnNavigationDemo/src/DetailScreen.js
+++ rnNavigationDemo/src/DetailScreen.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 export default class DetailScreen extends Component {
   constructor(props) {
@@ -13,10 +16,45 @@ export default class DetailScreen extends Component {
       alert(JSON.stringify(item))
   }
 
+  onClickBack = () => {
+    this.props.navigation.navigate("Home"); //refer to name of navigation stack
+  }
+
   render() {
     return (
-      <View>
+      <View style={{flex:1, flexDirection: 'column', alignItems: 'center'}}> 
+        <View style={{flex: 0.2}} >
         <Text> DetailScreen </Text>
+        <Button title="Back" onPress={this.onClickBack}></Button>
+        </View>
+        <View style={{flex: 0.8, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}} >
+        <Avatar
+          size="small"
+          rounded
+          title="MT"
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+        />
+        <Avatar
+          size="medium"
+          title="BP"
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+        />
+        <Avatar
+          size="large"
+          title="LW"
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+        />
+        <Avatar
+          size="xlarge"
+          rounded
+          title="CR"
+          onPress={() => console.log("Works!")}
+          activeOpacity={0.7}
+        />
+      </View>
       </View>
     );
   }
