ba4744206196584fc4236cbfc241025a24067ab4
tok0126 <tok0126@hotmail.com>
2019-10-16 16:03:15 +0700

    post get

--- rnNavigationDemo/src/Tab1Screen.js
+++ rnNavigationDemo/src/Tab1Screen.js
@@ -44,14 +44,52 @@ export default class Tab1Screen extends Component {
     });
   }
 
+  callGet = () => {
+    const data = {
+      username: 'admin',
+      password: 'password',
+      type: 'foods'
+    }
+    axios.get('http://codemobiles.com/adhoc/youtubes/index_new.php', { params: data })
+    .then(response => {
+      alert(response.data.youtubes[0].title)
+    })
+    .catch(err => {
+      alert(JSON.stringify(err));
+    })
+  }
+
+  callPost = () => {
+    const data = new FormData();
+    data.append('username', 'admin');
+    data.append('password', 'password');
+    data.append('type', 'foods');
+    axios.post('http://codemobiles.com/adhoc/youtubes/index_new.php', data)
+    .then(response => {
+      alert(response.data.youtubes[1].title);
+    })
+    .catch(err => {
+      alert(JSON.stringify(err));
+    })
+  }
+
   render() {
     return (
-      <View>
+      <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "space-around"
+      }}
+      >
         <Text> Tab1 </Text>
         <Button title="GO" onPress={this.onClickDetail}/>
 
         <Button title="fetch" onPress={this.fetchSomething}/>
         <Button title="fetch with axios" onPress={this.fetchAxios}/>
+
+        <Button title="call get" onPress={this.callGet}/>
+        <Button title="call post" onPress={this.callPost}/>
       </View>
     );
   }
