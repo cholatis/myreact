63f3715d99cd063045fa83c4716fc8c6906e762d
Cholatis Srikanthimaraks <cholatissrikanthimaraks@Cholatiss-MacBook-Pro.local>
2019-10-16 07:29:12 +0700

    networking fetch

--- rnNavigationDemo/src/Tab1Screen.js
+++ rnNavigationDemo/src/Tab1Screen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import axios from 'axios';
 
 
 export default class Tab1Screen extends Component {
@@ -21,11 +22,36 @@ export default class Tab1Screen extends Component {
     this.props.navigation.navigate('Detail', {item})
   }
 
+  //original fetch
+  fetchSomething = () => {
+    fetch('https://facebook.github.io/react-native/movies.json')
+    .then(result => result.json())
+    .then(jsonResult => {
+      alert("First movie in array: " + jsonResult.movies[0].title);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  fetchAxios = () => {
+    axios.get('https://facebook.github.io/react-native/movies.json')
+    .then(response => {
+      alert(response.data.movies[0].title)
+    })
+    .catch(err=> {
+      console.log(err);
+    });
+  }
+
   render() {
     return (
       <View>
         <Text> Tab1 </Text>
         <Button title="GO" onPress={this.onClickDetail}/>
+
+        <Button title="fetch" onPress={this.fetchSomething}/>
+        <Button title="fetch with axios" onPress={this.fetchAxios}/>
       </View>
     );
   }
