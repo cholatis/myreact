00914585b1d2ed6d8b18df307b134c3f201ecdf3
Cholatis Srikanthimaraks <cholatissrikanthimaraks@Cholatiss-MacBook-Pro.local>
2019-10-17 07:47:14 +0700

    JSON feed prototype

--- rnNavigationDemo/src/Tab2Screen.js
+++ rnNavigationDemo/src/Tab2Screen.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, Image, ImageBackground } from 'react-native';
+import { Card } from 'react-native-elements';
 
-export default class Tab2Screen extends Component {
+class Tab2Screen extends Component {
   constructor(props) {
     super(props);
     this.state = {
     };
   }
 
+  renderItemList = (item) => {
+    return(
+      <View>
+      <Card containerStyle={{overflow: 'hidden', flexDirection: 'column', marginBottom: 20, borderRadius: 8, padding: 0}}>
+        <View style={{flexDirection: 'row', marginBottom: 16, height: 45, alignItems: 'center'}} >
+          <Image source={require('./assets/img/avatar.png')} style={{ width: 45, height: '100%', marginRight: 16}} />
+          <View style ={{flexDirection: 'column'}} >
+            <Text style={{fontWeight: '700'}}>Title</Text>
+            <Text  style={{fontWeight: '100'}}>Subtitle</Text>
+          </View>
+        </View>
+        <Image source={require('./assets/img/loadingimg.png')} style={{width: '100%', height: 200}} />
+      </Card>
+      </View>
+    )
+  }
+
+  renderHeaderList = () => {
+    return(
+      <Image 
+      resizeMode='contain'
+      style={{width: '100%', height: 70, marginBottom: 20 }} 
+      source={ require('./assets/img/header_react_native.png')} />
+    ) 
+  }
+
   render() {
     return (
-      <View>
-        <Text> Tab2 </Text>
+      <View style={{flex: 1}}>
+        <ImageBackground
+        style={{width: '100%', height: '100%'}} 
+        source={require('./assets/img/bg.png')}>
+        <FlatList 
+          ListHeaderComponent = { this.renderHeaderList}
+          style={{marginTop : 10, marginLeft: 15, marginRight: 15}}
+          data={[1,2,3,4]}
+          renderItem={({item}) => this.renderItemList(item)}
+        />
+        </ImageBackground>
       </View>
     );
   }
 }
+
+export default Tab2Screen;
